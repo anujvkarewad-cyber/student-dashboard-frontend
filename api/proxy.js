201fdd1bd39da36df4e309e4755195e7bbc04441
anujvkarewad-cyber <anujvkarewad@gmail.com>
2026-07-22 19:14:13 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,21 @@
 export default async function handler(req, res) {
   try {
-  await fetch(process.env.APPS_SCRIPT_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(req.body)
-      }
-    );
+
+    const response = await fetch(process.env.APPS_SCRIPT_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
 
     const data = await response.json();
 
-    res.status(200).json(data);
+    return res.status(200).json(data);
 
   } catch (err) {
 
-    res.status(500).json({
+    return res.status(500).json({
       error: err.message
     });
 
